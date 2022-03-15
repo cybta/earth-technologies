29fb4c8ff83311a5d98a3d13ba34faad95d1261d
cybta <c.saad@live.com>
2022-03-15 13:25:27 +0300

    preload images fix carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -42,86 +42,88 @@ export default class InitCarousel {
         imgpreload.decode();
       })
 
-      listThumbs.map((item, index) => {
-        carouselSlider.innerHTML += `<div id="thumb-${index}" data-thumb="${index}" class="thumbnail"><img src="${item}" /></div>`;
-      });
-
-      const thumbId = 'thumb-' + focusedElIndex
-      document.getElementById(thumbId).classList.add('active')
-
-      carouselSlider.innerHTML;
-      let direction;
-      const carouselWidth = this.carouselWidth ? this.carouselWidth : 1600;
-
-      const slidesToShow = this.slidesToSHow ? this.slidesToSHow : 3;
-      const thumbWidh = carouselWidth / slidesToShow;
-
-      carousel.style.width = carouselWidth + "px";
-      nav_cont.style.width = carouselWidth + "px";
-      carouselSlider.style.width = carouselWidth * listThumbs.length + "px";
-
-      const itemsList = document.querySelectorAll(".thumbnail");
-
-      for (let i = 0; i < itemsList.length; i++) {
-        itemsList[i].style.width = thumbWidh + "px";
-        itemsList[i].style.flexBasis = thumbWidh + "px";
-      }
-
-      prev.addEventListener("click", function () {
-        if (direction === "next") {
-          carouselSlider.appendChild(carouselSlider.firstElementChild);
-          direction = "prev";
-        } else if (direction === undefined) {
-          carouselSlider.appendChild(carouselSlider.firstElementChild);
-          direction = "prev";
-        }
-  
-        if (focusedElIndex > 0) {
-          focusedElIndex--;
-        } else {
-          focusedElIndex = listThumbs.length - 1;
-        }
-  
-        carouselSlider.style.transform = `translateX(${thumbWidh}px)`;
-        // carousel.style.justifyContent = 'flex-end'
-      });
-  
-      next.addEventListener("click", function () {
-        direction = "next";
-  
-        if (focusedElIndex < listThumbs.length - 1) {
-          focusedElIndex++;
-        } else {
-          focusedElIndex = 0;
-        }
+      setTimeout(() => {
+        array.map((item, index) => {
+          carouselSlider.innerHTML += `<div id="thumb-${index}" data-thumb="${index}" class="thumbnail"><img src="${item}" /></div>`;
+        });
   
         const thumbId = 'thumb-' + focusedElIndex
-        const allThumbs = document.querySelectorAll('.thumbnail')
-        allThumbs.forEach(thumb => {
-          thumb.classList.remove('active')
-        })
-        
         document.getElementById(thumbId).classList.add('active')
   
-        console.log("focused Thumb is " + thumbId);
+        carouselSlider.innerHTML;
+        let direction;
+        const carouselWidth = this.carouselWidth ? this.carouselWidth : 1600;
   
-        carouselSlider.style.transform = `translateX(-${thumbWidh}px)`; // translateX value should be calculated dinamicaly
-        carousel.style.justifyContent = "flex-start";
-      });
+        const slidesToShow = this.slidesToSHow ? this.slidesToSHow : 3;
+        const thumbWidh = carouselWidth / slidesToShow;
   
-      carouselSlider.addEventListener("transitionend", function () {
-        if (direction === "next") {
-          carouselSlider.appendChild(carouselSlider.firstElementChild);
-        } else if (direction === "prev") {
-          carouselSlider.prepend(carouselSlider.lastElementChild);
+        carousel.style.width = carouselWidth + "px";
+        nav_cont.style.width = carouselWidth + "px";
+        carouselSlider.style.width = carouselWidth * listThumbs.length + "px";
+  
+        const itemsList = document.querySelectorAll(".thumbnail");
+  
+        for (let i = 0; i < itemsList.length; i++) {
+          itemsList[i].style.width = thumbWidh + "px";
+          itemsList[i].style.flexBasis = thumbWidh + "px";
         }
   
-        carouselSlider.style.transition = "0s";
-        carouselSlider.style.transform = "translate(0)";
-        setTimeout(() => {
-          carouselSlider.style.transition = "all .5s";
+        prev.addEventListener("click", function () {
+          if (direction === "next") {
+            carouselSlider.appendChild(carouselSlider.firstElementChild);
+            direction = "prev";
+          } else if (direction === undefined) {
+            carouselSlider.appendChild(carouselSlider.firstElementChild);
+            direction = "prev";
+          }
+    
+          if (focusedElIndex > 0) {
+            focusedElIndex--;
+          } else {
+            focusedElIndex = listThumbs.length - 1;
+          }
+    
+          carouselSlider.style.transform = `translateX(${thumbWidh}px)`;
+          // carousel.style.justifyContent = 'flex-end'
         });
-      });
+    
+        next.addEventListener("click", function () {
+          direction = "next";
+    
+          if (focusedElIndex < listThumbs.length - 1) {
+            focusedElIndex++;
+          } else {
+            focusedElIndex = 0;
+          }
+    
+          const thumbId = 'thumb-' + focusedElIndex
+          const allThumbs = document.querySelectorAll('.thumbnail')
+          allThumbs.forEach(thumb => {
+            thumb.classList.remove('active')
+          })
+          
+          document.getElementById(thumbId).classList.add('active')
+    
+          console.log("focused Thumb is " + thumbId);
+    
+          carouselSlider.style.transform = `translateX(-${thumbWidh}px)`; // translateX value should be calculated dinamicaly
+          carousel.style.justifyContent = "flex-start";
+        });
+    
+        carouselSlider.addEventListener("transitionend", function () {
+          if (direction === "next") {
+            carouselSlider.appendChild(carouselSlider.firstElementChild);
+          } else if (direction === "prev") {
+            carouselSlider.prepend(carouselSlider.lastElementChild);
+          }
+    
+          carouselSlider.style.transition = "0s";
+          carouselSlider.style.transform = "translate(0)";
+          setTimeout(() => {
+            carouselSlider.style.transition = "all .5s";
+          });
+        });
+      },1000)
       
       console.log('all set')
     }
